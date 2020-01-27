ce414b30e3553f6a0edd892d02c8ee27bae779fd
420MemesxD <55286185+420MemesxD@users.noreply.github.com>
2020-01-27 13:47:14 -0500

    added requirements for lab3

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Student Will = new Student("William", "Van Wieringen", 1111523);
+        System.out.println(Will.toString());
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,56 @@
 public class Student {
 
-    private String firstName = "William";
-    private String lastName = "Van Wieringen";
-    private int studentNumber = 1111523;
+
+
+    private String firstName;
+    private String lastName;
+    private int studentNumber;
+
+
+    public Student(String firstName, String lastName, int studentNumber) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setStudentNumber(studentNumber);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        firstName.charAt(1);
+        if (firstName.length() > 1 && Character.isUpperCase(firstName.codePointAt(0)))
+        this.firstName = firstName;
+        else
+            throw new IllegalArgumentException("name length must be longer than one letter and first letter must be upper case");
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+
+        if (lastName.length() > 1 && Character.isUpperCase(lastName.codePointAt(0)))
+        this.lastName = lastName;
+        else
+            throw new IllegalArgumentException("last name length must be longer than one letter and first letter must be upper case");
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        if (studentNumber < 10000000 || studentNumber > 9999999)
+        this.studentNumber = studentNumber;
+        else
+            throw new IllegalArgumentException("must be in range of 10000000-9999999");
+    }
+
+    public String toString()
+    {
+        return String.format("%s %s student #: %d", firstName, lastName, studentNumber);
+    }
 
 }
