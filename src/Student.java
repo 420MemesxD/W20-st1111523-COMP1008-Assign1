2134904dee2a3ab8f0fecc1f44ca5449e78323f6
420MemesxD <55286185+420MemesxD@users.noreply.github.com>
2020-02-17 14:52:18 -0500

    created layout for GUI and added an image of myself and text for first name, last name and student number.

--- src/Student.java
+++ src/Student.java
@@ -11,6 +11,8 @@ public class Student {
         setStudentNumber(studentNumber);
     }
 
+
+
     public String getFirstName() {
         return firstName;
     }
--- src/StudentViewController.java
+++ src/StudentViewController.java
@@ -1,12 +1,35 @@
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class StudentViewController implements Initializable {
+    @FXML private Label fName;
+    @FXML private Label lName;
+    @FXML private Label sNumber;
+    @FXML private ImageView myImage;
+    private Student card;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        card = new Student("William", "Van Wieringen", 1111523);
+        myImage.setImage(new Image("./images/image0.jpg"));
+        updateInfo();
+
+
+    }
+
+    @FXML
+    private void updateInfo()
+    {
+        fName.setText("First Name: " + card.getFirstName());
+        lName.setText("Last Name: " + card.getLastName());
+        sNumber.setText("Student Number: " + card.getStudentNumber());
 
     }
 }
